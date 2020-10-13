e43f46303884d27c6eeb002af1b6035bdddb4930
Muhammad Sajjad <sajjadc201@gmail.com>
2020-10-13 23:36:56 +0500

    app and home change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
-import {Home} from './Home';
 import {Product} from './Product';
 import { ProductIndex } from "./ProductIndex";
 import {LaunchShoe} from './LaunchShoe';
@@ -9,17 +8,15 @@ export const App = () => {
     return (
         <div>
             <nav>
-                <Link to='/'>Home</Link>
                 {/* <Link to='./about'>About</Link> */}
-                <Link to='./product'>Product</Link>
+                <Link to='/'>Product</Link>
 
             </nav>
             <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/product' element={<Product />}>
-                    <Route path='/' element={<ProductIndex />} />
+                <Route path='/' element={<Product />}>
+                    <Route  path='/' element={<ProductIndex />} />
                     <Route path=':slug' element={<LaunchShoe />} />
-                    </Route>
+                </Route>
             </Routes>
         </div>
     )
--- src/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Home = () => {
-    return (
-        <div>
-            <h3>this is home</h3>
-        </div>
-    )
-}
